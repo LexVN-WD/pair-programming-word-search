9fa86da816ee331b782aaf0fcb89ea760e533d3a
Alexander <apvannug.lhl.webdev@gmail.com>
2022-12-17 23:50:55 -0700

    refactor wordSearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const horizontalJoin = letters.map(ls => ls.join(''));
+
+    const transpose = letters[0].map((column, element) => letters.map(row => row[element]));
+   
+    const verticalJoin = transpose.map(ls => ls.join(''));
+
+    if (word.length === 0 || letters.length === 0) {
+        return false;
     }
-}
+
+    for (l of horizontalJoin) {
+        if (l === word) {
+            return true;
+        }
+        for (l of verticalJoin) {
+            if (l === word) {
+                return true;
+            }
+        }
+    }   
+    return false;
+};
 
 module.exports = wordSearch
